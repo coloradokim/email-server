22381f1b8a000e7b0d0b76fa5f7bc3dd99cb53f0
coloradokim <schlesinger.kim@gmail.com>
2017-09-18 09:11:22 -0600

    set up knex

--- app.js
+++ app.js
@@ -5,14 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var userEmails = require('./routes/user_emails');
+var users = require('./routes/users')
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +18,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/v1/api/', userEmails);
+app.use('v1/api/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
