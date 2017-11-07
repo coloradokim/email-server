0937269906370a61d28e2a7ee20ff30b0a2973c9
coloradokim <schlesinger.kim@gmail.com>
2017-11-06 20:02:02 -0700

    add node version

--- migrations/20171106081712_create_email_phone.js
+++ migrations/20171106081712_create_email_phone.js
@@ -2,7 +2,7 @@ exports.up = (knex, Promise) => {
   return knex.schema.createTable('email_phone', (table) => {
     table.increments();
     table.string('email').unique().notNullable();
-    table.varchar('phone').unique().notNullable();
+    table.varchar('phone')
     table.timestamp('created_at').defaultTo(knex.fn.now());
   });
 };
