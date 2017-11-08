9f24f1bd5a1a2067102db57a7d63b110a99bef70
coloradokim <schlesinger.kim@gmail.com>
2017-11-07 22:41:54 -0700

    update request parameters

--- migrations/20171107203427_create_first_contact_info.js
+++ migrations/20171107203427_create_first_contact_info.js
@@ -1,9 +1,9 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('first_contact_info', (table) => {
     table.increments().notNullable;
-    table.text('first_name');
-    table.text('email').unique().notNullable();
-    table.text('phone');
+    table.string('first_name');
+    table.string('email').unique().notNullable();
+    table.string('phone');
     table.timestamp('created_at').defaultTo(knex.fn.now());
   });
 };
--- routes/first_contact_info.js
+++ routes/first_contact_info.js
@@ -3,10 +3,12 @@ var router = express.Router();
 var dbConnection = require('../db/knex');
 
 router.post('/', (req, res, next) => {
+  const newName = req.body.first_name;
   const newEmail = req.body.email;
   const newPhone = req.body.phone;
   dbConnection('first_contact_info')
     .insert({
+      first_name: newName,
       email: newEmail,
       phone: newPhone
     })
