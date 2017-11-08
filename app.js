18467fde32d99737538c01613299071bdc5e9cf4
coloradokim <schlesinger.kim@gmail.com>
2017-11-07 22:28:08 -0700

    update

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var app = express();
 
 var first_contact_info = require('./routes/first_contact_info');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
