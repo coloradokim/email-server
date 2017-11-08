5780917dfaa6f0600c5da13c7f8197e501a20472
coloradokim <schlesinger.kim@gmail.com>
2017-11-07 21:33:52 -0700

    update schema with first name column

--- migrations/20171107203427_create_first_contact_info.js
+++ migrations/20171107203427_create_first_contact_info.js
@@ -1,8 +1,9 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('first_contact_info', (table) => {
     table.increments();
+    table.string('first_name');
     table.string('email').unique().notNullable();
-    table.varchar('phone')
+    table.string('phone');
     table.timestamp('created_at').defaultTo(knex.fn.now());
   });
 };
--- seeds/01_first_contact_info.js
+++ seeds/01_first_contact_info.js
@@ -2,8 +2,8 @@ exports.seed = function(knex, Promise) {
   return knex('first_contact_info').del()
     .then(function () {
       return knex('first_contact_info').insert([
-        {email: 'kim@hirediversity.us', phone: '7194294831'},
-        {email: 'nina@hirediversity.us', phone: '7032200704'},
+        {first_name: 'kim', email: 'kim@hirediversity.us', phone: '7194294831'},
+        {first_name: 'nina', email: 'nina@hirediversity.us', phone: '7032200704'},
       ]);
     });
 };
